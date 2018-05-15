6d1f1034f0cb2b4599a3b9feb51defb187faf40a
sajan.appsplorer <appsplorer.tech.sajan@gmail.com>
2018-05-16 02:06:32 +0600

    user auth blade and user and role table done

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+use App\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //$this->call(PermissionTableSeeder::class);
+        DB::table('users')->delete();
+
+        // create admin role
+        $role = ['name' => 'admin', 'display_name' => 'Admin', 'description' => 'Full Permission'];
+        $role = Role::create($role);
+        //set role permission
+        $permission = Permission::get();
+        foreach ($permission as $key => $value) {
+            $role->attachPermission($value);
+        }
+        // create admin user
+        $user = ['name' => 'Admin User', 'email' => 'adminuser@test.com', 'password' => Hash::make('adminpwd')];
+        $user = User::create($user);
+        // set user roll
+        $user->attachRole($role);
     }
 }
